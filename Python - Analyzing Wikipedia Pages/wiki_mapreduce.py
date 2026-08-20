75f189dfdf005ac337dd60546953bec0536b0b23
puney <jainpuney@gmail.com>
2026-08-20 14:52:16 +0100

    Added more wiki files, and added the start of the multiprocessing code.

---
+++ Python - Analyzing Wikipedia Pages/wiki_mapreduce.py
@@ -0,0 +1,11 @@
+import os
+
+def mapper(file_chunk):
+    line_count = []
+    for file in file_chunk:
+        with open(os.path.join("wiki", file), encoding="UTF-8") as f:
+            line_count.append(len(f.readlines()))
+    return sum(line_count), len(file_chunk)
+
+def reducer(a, b):
+    return a + b
